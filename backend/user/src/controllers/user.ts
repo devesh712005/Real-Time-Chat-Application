14d04d74e200f8744fcd13daab992e991dbfdf19
dev <devesh712005@gmail.com>
2026-02-18 10:56:14 +0530

    feat(user-service): add authentication with Redis OTP and RabbitMQ initialization

---
+++ backend/user/src/controllers/user.ts
@@ -0,0 +1,73 @@
+import { generateToken } from "../config/generateToken.js";
+import { publishToQueue } from "../config/rabbitmq.js";
+import TryCatch from "../config/TryCatch.js";
+import { redisClient } from "../index.js";
+import { User } from "../model/User.js";
+
+export const loginUser = TryCatch(async (req, res) => {
+  const { email } = req.body;
+
+  const rateLimitKey = `otp:ratelimit:${email}`;
+  const rateLimit = await redisClient.get(rateLimitKey);
+  if (rateLimit) {
+    res.status(429).json({
+      message: "Too many requests. Please wait before requesting new otp",
+    });
+    return;
+  }
+
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  const otpKey = `otp:${email}`;
+
+  await redisClient.set(otpKey, otp, {
+    EX: 300,
+  });
+  await redisClient.set(rateLimitKey, "true", {
+    EX: 60,
+  });
+
+  const message = {
+    to: email,
+    subject: "Your otp code",
+    body: `Your otp is ${otp}.It is valid for 5 minutes`,
+  };
+
+  await publishToQueue("send-otp", message);
+  res.status(200).json({
+    message: "OTP sent to your mail",
+  });
+});
+
+export const verifyUser = TryCatch(async (req, res) => {
+  const { email, otp: enteredOtp } = req.body;
+  if (!email || !enteredOtp) {
+    res.status(400).json({
+      message: "Email and otp required",
+    });
+    return;
+  }
+
+  const otpKey = `otp:${email}`;
+
+  const storedOtp = await redisClient.get(otpKey);
+
+  if (!storedOtp || storedOtp !== enteredOtp) {
+    res.status(400).json({
+      message: "Invalid or expired OTP",
+    });
+    return;
+  }
+
+  await redisClient.del(otpKey); //If otp is correct
+  let user = await User.findOne({ email });
+  if (!user) {
+    const name = email.slice(0, 8);
+    user = await User.create({ name, email });
+  }
+  const token = generateToken(user);
+  res.json({
+    message: "User verified",
+    user,
+    token,
+  });
+});
---
+++ backend/user/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { createClient } from "redis";
+import userRoutes from "./routes/user.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+connectDb();
+connectRabbitMQ();
+
+app.use("/api/v1", userRoutes);
+
+const redisUrl = process.env.REDIS_URL;
+if (!redisUrl) {
+  throw new Error("REDIS_URL missing in .env");
+}
+export const redisClient = createClient({ url: redisUrl });
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to redis"))
+  .catch(console.error);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
