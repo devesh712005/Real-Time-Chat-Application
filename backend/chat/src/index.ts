9bb62c8b24cc9773912a3032c71d278d8a6b88a9
dev <devesh712005@gmail.com>
2026-02-23 19:05:32 +0530

    fix(chat): configure CORS for chat service

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import chatRoutes from "./routes/chat.js";
+import cors from "cors";
 
 dotenv.config();
 connectDb();
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", chatRoutes);
 const PORT = process.env.PORT;
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -9,6 +9,7 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 dotenv.config();
 connectDb();
 connectRabbitMQ();
